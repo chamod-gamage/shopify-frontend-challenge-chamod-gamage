037f5ebdb763c2a20acccaba250190c794faee45
chamod-gamage <chamodgamage26@gmail.com>
2021-01-09 16:57:27 -0500

    Secure HTTP call to OMDB

--- src/api/search.js
+++ src/api/search.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const searchOMDB = async ({ searchTerm, year }) => {
-  let res = await axios.get(`http://www.omdbapi.com/`, {
+  let res = await axios.get(`https://www.omdbapi.com/`, {
     params: {
       apikey: process.env.REACT_APP_OMDB_KEY,
       s: searchTerm,
