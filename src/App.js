115ccd7f077e18e8353b7fece7493bfc7f6b7bec
chamod-gamage <chamodgamage26@gmail.com>
2021-02-08 23:43:57 -0500

    Introduce top spacing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Container>
+            <br />
             <h1 className="gold">
               <i>The Nominator</i>
             </h1>
